9af4f835b87758ccf4339bc77a90c284d3c25419
aditya24736 <97228657+aditya24736@users.noreply.github.com>
2024-10-29 02:01:03 +0530

    Add files via upload

---
+++ airlinemanagementsystem/Conn.java
@@ -0,0 +1,18 @@
+package airlinemanagementsystem;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    public Conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///airlinemanagementsystem", "root", "Aditya2424");
+            s = c.createStatement();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
